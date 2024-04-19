8c784f6f14e2b7330e49be28a8c4e4799cb65fc0
Ram Manthry <ram.manthry@gmail.com>
2024-04-19 16:50:21 +1200

    fixed proxy

--- Frontend/src/components/ShowToDo.tsx
+++ Frontend/src/components/ShowToDo.tsx
@@ -4,77 +4,70 @@ import axios from 'axios';
 import { TodoItem } from '../types/ToDo';
 
 export const ShowToDo = () => {
-	const [items, setItems] = useState<TodoItem[]>([]);
+  const [items, setItems] = useState<TodoItem[]>([]);
 
-	useEffect(() => {
-		getItems();
-	}, []);
+  useEffect(() => {
+    getItems();
+  }, []);
 
-	async function getItems() {
-		try {
-			const response = await axios.get('http://localhost:7002/api/todoItems/');
-			setItems(response.data);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+  async function getItems() {
+    try {
+      const response = await axios.get('/api/todoItems');
+      setItems(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	async function handleMarkAsComplete(item: TodoItem) {
-		try {
-			const response = await axios.put(
-				`http://localhost:7002/api/todoItems/${item.id}`,
-				{
-					...item,
-					isCompleted: true,
-				}
-			);
-			if (response.status === 200 || response.status === 201) {
-				getItems();
-			}
-			getItems();
-		} catch (error) {
-			console.error(error);
-		}
-	}
+  async function handleMarkAsComplete(item: TodoItem) {
+    try {
+      const response = await axios.put(`/api/todoItems/${item.id}`, {
+        ...item,
+        isCompleted: true,
+      });
+      if (response.status === 200 || response.status === 201) {
+        getItems();
+      }
+      getItems();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-	return (
-		<Row className="mt-2">
-			<Col>
-				<h1>
-					Showing {items.length} Item(s){' '}
-					<Button variant="primary" className="pull-right" onClick={getItems}>
-						Refresh
-					</Button>
-				</h1>
-				<Table striped bordered hover>
-					<thead>
-						<tr>
-							<th>Id</th>
-							<th>Description</th>
-							<th>Action</th>
-						</tr>
-					</thead>
-					<tbody>
-						{items.map((item) => (
-							<tr key={item.id}>
-								<td>{item.id}</td>
-								<td>{item.description}</td>
-								{!item.isCompleted && (
-									<td>
-										<Button
-											variant="warning"
-											size="sm"
-											onClick={() => handleMarkAsComplete(item)}
-										>
-											Mark as completed
-										</Button>
-									</td>
-								)}
-							</tr>
-						))}
-					</tbody>
-				</Table>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row className="mt-2">
+      <Col>
+        <h1>
+          Showing {items.length} Item(s){' '}
+          <Button variant="primary" className="pull-right" onClick={getItems}>
+            Refresh
+          </Button>
+        </h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.description}</td>
+                {!item.isCompleted && (
+                  <td>
+                    <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
+                      Mark as completed
+                    </Button>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  );
 };
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,14 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'https://localhost:7002',
-				changeOrigin: true,
-				secure: false,
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:7002',
+    },
+  },
 });
