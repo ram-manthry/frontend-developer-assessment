5960ef17f9ea7c58d9b77014543552d89e6be83c
Ram Manthry <ram.manthry@gmail.com>
2024-04-19 17:23:09 +1200

    fixed vite config for tests

---
+++ Frontend/src/tests/App.spec.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from '../App';
+
+test('renders the footer text', () => {
+  render(<App />);
+  const footerElement = screen.getByText(/clearpoint.digital/i);
+  expect(footerElement).toBeInTheDocument();
+});
