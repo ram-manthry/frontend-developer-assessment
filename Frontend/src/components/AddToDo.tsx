0f6135ad2d37fa31712a80fe9b70a7cc903afacf
Ram Manthry <ram.manthry@gmail.com>
2024-04-19 16:42:10 +1200

    changed to vite to fix packages compatibility issues

--- Frontend/src/components/AddToDo.tsx
+++ Frontend/src/components/AddToDo.tsx
@@ -4,70 +4,79 @@ import axios from 'axios';
 import { ErrorMessage } from './ErrorMessage';
 
 type Props = {
-  onAdd: () => void;
+	onAdd: () => void;
 };
 
 export const AddToDo = ({ onAdd }: Props) => {
-  const [description, setDescription] = useState('');
-  const [error, setError] = useState('');
-  const handleDescriptionChange = (event: { target: { value: React.SetStateAction<string> } }) => {
-    setDescription(event.target.value);
-  };
+	const [description, setDescription] = useState('');
+	const [error, setError] = useState('');
+	const handleDescriptionChange = (event: {
+		target: { value: React.SetStateAction<string> };
+	}) => {
+		setDescription(event.target.value);
+	};
 
-  async function handleAdd() {
-    try {
-      const response = await axios.post('http://localhost:7002/api/todoItems/', {
-        description,
-        isCompleted: false,
-      });
-      if (response.status === 200) {
-        setDescription('');
-        onAdd();
-      }
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        setError(error.response?.data);
-      }
-    }
-  }
+	async function handleAdd() {
+		try {
+			const response = await axios.post(
+				'http://localhost:7002/api/todoItems/',
+				{
+					description,
+					isCompleted: false,
+				}
+			);
+			if (response.status === 200 || response.status === 201) {
+				setDescription('');
+				onAdd();
+			}
+		} catch (error) {
+			if (axios.isAxiosError(error)) {
+				setError(error.response?.data);
+			}
+		}
+	}
 
-  function handleClear() {
-    setDescription('');
-  }
+	function handleClear() {
+		setDescription('');
+	}
 
-  return (
-    <>
-      <Row>
-        <Col>
-          <Container>
-            <h1>Add Item</h1>
-            <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
-              <Form.Label column sm="2">
-                Description
-              </Form.Label>
-              <Col md="6">
-                <Form.Control
-                  type="text"
-                  placeholder="Enter description..."
-                  value={description}
-                  onChange={handleDescriptionChange}
-                />
-              </Col>
-            </Form.Group>
-            <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
-              <Stack direction="horizontal" gap={2}>
-                <Button variant="primary" onClick={handleAdd}>
-                  Add Item
-                </Button>
-                <Button variant="secondary" onClick={handleClear}>
-                  Clear
-                </Button>
-              </Stack>
-            </Form.Group>
-          </Container>
-        </Col>
-      </Row>
-      <ErrorMessage message={error} onClose={() => setError('')} />
-    </>
-  );
+	return (
+		<>
+			<Row>
+				<Col>
+					<Container>
+						<h1>Add Item</h1>
+						<Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
+							<Form.Label column sm="2">
+								Description
+							</Form.Label>
+							<Col md="6">
+								<Form.Control
+									type="text"
+									placeholder="Enter description..."
+									value={description}
+									onChange={handleDescriptionChange}
+								/>
+							</Col>
+						</Form.Group>
+						<Form.Group
+							as={Row}
+							className="mb-3 offset-md-2"
+							controlId="formAddTodoItem"
+						>
+							<Stack direction="horizontal" gap={2}>
+								<Button variant="primary" onClick={handleAdd}>
+									Add Item
+								</Button>
+								<Button variant="secondary" onClick={handleClear}>
+									Clear
+								</Button>
+							</Stack>
+						</Form.Group>
+					</Container>
+				</Col>
+			</Row>
+			<ErrorMessage message={error} onClose={() => setError('')} />
+		</>
+	);
 };
--- Frontend/src/components/Instructions.tsx
+++ Frontend/src/components/Instructions.tsx
@@ -1,25 +1,34 @@
-import { Alert } from 'react-bootstrap';
+import Alert from 'react-bootstrap/Alert';
 
 export const Instructions = () => {
-  return (
-    <div>
-      <Alert variant="success">
-        <Alert.Heading>Todo List App</Alert.Heading>
-        Welcome to the ClearPoint frontend technical test. We like to keep things simple, yet clean so your task(s) are
-        as follows:
-        <br />
-        <br />
-        <ol className="list-left">
-          <li>Add the ability to add (POST) a Todo Item by calling the backend API</li>
-          <li>Display (GET) all the current Todo Items in the below grid and display them in any order you wish</li>
-          <li>
-            Bonus points for completing the 'Mark as completed' button code for allowing users to update and mark a
-            specific Todo Item as completed and for displaying any relevant validation errors/ messages from the API in
-            the UI
-          </li>
-          <li>Feel free to add unit tests and refactor the component(s) as best you see fit</li>
-        </ol>
-      </Alert>
-    </div>
-  );
+	return (
+		<div>
+			<Alert variant="success">
+				<Alert.Heading>Todo List App</Alert.Heading>
+				Welcome to the ClearPoint frontend technical test. We like to keep
+				things simple, yet clean so your task(s) are as follows:
+				<br />
+				<br />
+				<ol className="list-left">
+					<li>
+						Add the ability to add (POST) a Todo Item by calling the backend API
+					</li>
+					<li>
+						Display (GET) all the current Todo Items in the below grid and
+						display them in any order you wish
+					</li>
+					<li>
+						Bonus points for completing the 'Mark as completed' button code for
+						allowing users to update and mark a specific Todo Item as completed
+						and for displaying any relevant validation errors/ messages from the
+						API in the UI
+					</li>
+					<li>
+						Feel free to add unit tests and refactor the component(s) as best
+						you see fit
+					</li>
+				</ol>
+			</Alert>
+		</div>
+	);
 };
